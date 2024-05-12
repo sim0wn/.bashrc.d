5e2e953f8753e584ad66a55a98cf27de18c370a1
Simon O'Brien <61605711+sim0wn@users.noreply.github.com>
2024-05-12 12:40:08 -0300

    adding sh files

---
+++ alias.sh
@@ -0,0 +1,5 @@
+alias pandoc='podman run --rm -v "$(pwd):/data:z" pandoc/latex'
+
+# Security Tools
+alias john='~/.local/share/john/run/john'
+alias rustscan='podman run --cap-add=NET_ADMIN --cap-add=NET_RAW --cap-add=NET_BIND_SERVICE -v "$(pwd):/data:z" -it --rm --name rustscan rustscan/rustscan:latest'
