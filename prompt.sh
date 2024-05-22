522ca11709b835f1c244642ed053fca770552407
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-05-22 12:19:20 -0300

    adding timestamp and git branch to PS1

--- prompt.sh
+++ prompt.sh
@@ -1,2 +1,11 @@
+time_stamp() {
+  local TIMESTAMP=$(date "+%d/%m/%y %H:%M:%S")
+  echo $TIMESTAMP
+}
+
+parse_git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1) /'
+}
+
 export PROMPT_DIRTRIM=3
-export PS1='\[\e[38;5;34m\]\u\[\e[38;5;220m\]@\[\e[38;5;32m\]\h\[\e[0m\] \[\e[38;5;251;4m\]\w\[\e[0m\] \[\e[38;5;129;1m\]\$\[\e[0m\] '
+export PS1='\[\e[38;5;34m\]\u\[\e[38;5;220m\]@\[\e[38;5;32m\]\h\[\e[0m\] \[\e[38;5;251;4m\]\w\[\e[0m\] [$(time_stamp)]\n$(parse_git_branch)\[\e[38;5;129;1m\]\$\[\e[0m\] '
